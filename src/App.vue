5d508cca0a5e4365588e06c17ee88330489560e8
RomiTT <chikyom.kim@gmail.com>
2018-12-02 22:46:03 +0900

    Implemented main layout with vuetify.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
-<template>
-  <v-app>
-    <div id="app">
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </div>
-      <v-content>
-        <h2>Loggined: {{$store.getters.isLoggined}}</h2>
-        <v-container fluid>
-          <router-view/>
-        </v-container>
-      </v-content>      
-    </div>
-  </v-app>
+<template>  
+  <div id="app">
+    <v-app>
+      <v-toolbar dark color="primary">
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+
+        <v-toolbar-title class="white--text">Title</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>apps</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>refresh</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>      
+
+      <v-container style="padding:0px;" fill-height fluid>
+        <v-layout>
+          <v-flex md3 side-bar>
+            <div id="nav">
+              <v-card dark color="yellow">
+                <v-card-text>
+                  <router-link to="/">Home</router-link>
+                </v-card-text>                
+              </v-card>
+              <v-card dark color="red">
+                <v-card-text>
+                  <router-link to="/about">About</router-link>
+                </v-card-text>                
+              </v-card>
+            </div>
+          </v-flex>
+          <v-flex main-content>
+            <v-content>
+              <h2>Loggined: {{$store.getters.isLoggined}}</h2>
+              <v-container fluid>
+                <router-view/>
+              </v-container>
+            </v-content>   
+          </v-flex>
+        </v-layout>
+      </v-container>   
+    </v-app>
+  </div>
 </template>
 
 <script>
@@ -48,6 +88,22 @@ export default {
 </script>
 
 <style>
+.content-root {
+  padding:0px;
+}
+.side-bar {
+  background:silver;
+}
+
+.main-content {
+  background:white;
+}
+
+.item {
+  margin:5px;
+}
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -56,12 +112,12 @@ export default {
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  padding:5px;
 }
 
 #nav a.router-link-exact-active {
